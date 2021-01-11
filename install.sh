f85bc8e550396558896a5d99e2ac00939212ec74
Dmitry Shilnenkov <dmitry.shilnenkov@emlid.com>
2021-01-11 17:52:43 +0300

    install: add python autocomplenction and histoty

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-echo "Prepare 'Vim'"
+echo "Preparing Vim..."
 curl -fLo vim/.vim/autoload/plug.vim --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo ""
 
-echo "Prepare 'Tmux'"
+echo "Preparing Tmux...'"
 rm -rf tmux/.tmux
 git clone https://github.com/tmux-plugins/tpm tmux/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tmux-resurrect.git \
@@ -16,20 +16,31 @@ git clone https://github.com/tmux-plugins/tmux-continuum \
     tmux/.tmux/plugins/tmux-continuum
 echo ""
 
-echo "Prepare 'Zsh'"
+echo "Preparing Zsh..."
 mkdir -p zsh/.antigen
 curl -L git.io/antigen > zsh/.antigen/antigen.zsh
 echo ""
 
-echo "Create symbols links"
-# stow vim tmux zsh
+echo "Prepearing Python..."
+# 'readline' module for Python 2
+sudo apt-get install libncurses5-dev
+sudo -H pip2 install readline
+
+echo "Creating symbols links..."
+# tmux
 ln -sf ${PWD}/tmux/.tmux ${HOME}/.tmux
 ln -sf ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
 
+# vim
 ln -sf ${PWD}/vim/.vim ${HOME}/.vim
 ln -sf ${PWD}/vim/.vimrc ${HOME}/.vimrc
 echo "Use 'PlugInstall' to install Vim plugins"
 
+# zsh
 ln -sf ${PWD}/zsh/.antigen ${HOME}/.antigen
 ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
+
+# python
+ln -sf ${PWD}/python/.pythonrc ${HOME}/.pythonrc
+echo "Use 'echo "export PYTHONSTARTUP=~/.pythonrc" >> ~/.profile' to use it at startup"
 

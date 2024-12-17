b593ccc3bf8a84745d509fd3b20d2dad1901a9da
Dmitry Shilnenkov <dmitry.shilnenkov@gmail.com>
2024-12-17 11:22:35 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -2,51 +2,57 @@
 
 set -e
 
+SCRIPT_DIR="$(dirname $(readlink -f ${0}))"
+
 echo "Preparing Vim..."
-curl -fLo vim/.vim/autoload/plug.vim --create-dirs \
+curl -fLo "${SCRIPT_DIR}/vim/autoload/plug.vim" --create-dirs \
      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo ""
 
 echo "Preparing Tmux...'"
-rm -rf tmux/.tmux
-git clone https://github.com/tmux-plugins/tpm tmux/.tmux/plugins/tpm
+rm -rf "${SCRIPT_DIR}/tmux/plugins"
+git clone https://github.com/tmux-plugins/tpm \
+    "${SCRIPT_DIR}/tmux/plugins/tpm"
 git clone https://github.com/tmux-plugins/tmux-resurrect.git \
-    tmux/.tmux/plugins/tmux-resurrect
+    "${SCRIPT_DIR}/tmux/plugins/tmux-resurrect"
 git clone https://github.com/tmux-plugins/tmux-continuum \
-    tmux/.tmux/plugins/tmux-continuum
-echo ""
+    "${SCRIPT_DIR}/tmux/plugins/tmux-continuum"
 
-echo "Preparing Zsh..."
-mkdir -p zsh/.antigen
-curl -L git.io/antigen-nightly > zsh/.antigen/antigen.zsh
-echo ""
+echo "Installing oh-my-zsh..."
+export ZSH="${SCRIPT_DIR}/zsh/oh-my-zsh"
+rm -rf "${ZSH}"
+curl -fLo /tmp/install.sh --create-dirs \
+     https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh /tmp/install.sh --unattended
 
-echo "Prepearing Python..."
-# 'readline' module for Python 2
-# sudo apt-get install libncurses5-dev
-# sudo -H pip2 install readline
+echo "Installing custom zsh plugins..."
+rm -rf "${ZSH}/custom/plugins/zsh-autosuggestions"
+rm -rf "${ZSH}/custom/plugins/zsh-completions"
+rm -rf "${ZSH}/custom/plugins/zsh-syntax-highlighting"
+
+git clone --quiet https://github.com/zsh-users/zsh-autosuggestions \
+   "${ZSH}/custom/plugins/zsh-autosuggestions"
+git clone --quiet https://github.com/zsh-users/zsh-completions \
+    "${ZSH}/custom/plugins/zsh-completions"
+git clone --quiet https://github.com/zsh-users/zsh-syntax-highlighting \
+    "${ZSH}/custom/plugins/zsh-syntax-highlighting"
+
+echo "Installing aphrodite theme..."
+rm -rf /tmp/aphrodite-terminal-theme
+git clone --quiet https://github.com/win0err/aphrodite-terminal-theme \
+   /tmp/aphrodite-terminal-theme
+cp /tmp/aphrodite-terminal-theme/aphrodite.zsh-theme \
+   "${ZSH}/custom/themes/aphrodite.zsh-theme"
 
 echo "Creating symbols links..."
 # tmux
-ln -sf ${PWD}/tmux/.tmux ${HOME}/.tmux
-ln -sf ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
-
+ln -sf ${SCRIPT_DIR}/tmux ${HOME}/.tmux
+ln -sf ${SCRIPT_DIR}/tmux/tmux.conf ${HOME}/.tmux.conf
 # vim
-ln -sf ${PWD}/vim/.vim ${HOME}/.vim
-ln -sf ${PWD}/vim/.vimrc ${HOME}/.vimrc
+ln -sf ${SCRIPT_DIR}/vim ${HOME}/.vim
+ln -sf ${SCRIPT_DIR}/vim/vimrc ${HOME}/.vimrc
 echo "Use 'PlugInstall' to install Vim plugins"
-
 # zsh
-ln -sf ${PWD}/zsh/.antigen ${HOME}/.antigen
-ln -sf ${PWD}/zsh/.zshrc ${HOME}/.zshrc
-
-# python
-ln -sf ${PWD}/python/.pythonrc ${HOME}/.pythonrc
-echo "Use 'echo "export PYTHONSTARTUP=~/.pythonrc" >> ~/.profile' to use it at startup"
-
+ln -sf ${SCRIPT_DIR}/zsh/zshrc ${HOME}/.zshrc
 #gdb
-ln -sf ${PWD}/gdb/.gdbinit ${HOME}/.gdbinit
-
-# i3
-# cd ${PWD}/i3 && bash install.sh
+ln -sf ${SCRIPT_DIR}/gdb/.gdbinit ${HOME}/.gdbinit
 
